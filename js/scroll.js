4fd36c487e2fef297ec775b288228a9bc9b009e1
ranierelacerda <ranimenezes@hotmail.com>
2016-02-03 15:08:22 -0200

    Mouse Wheel Smooth Developed
    -Visual Improvements;
    -Mouse Wheel Smooth Developed.

---
+++ js/scroll.js
@@ -0,0 +1,24 @@
+$(window).mousewheel(MouseWheelHandler);
+
+function MouseWheelHandler(e, delta) {
+	if (delta > 0){
+		var target = $('.current').data('previous');
+		if(target == null) return;
+		$.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: "#" + target
+		});
+		$('.current').removeClass('current');
+		$('#'+target).addClass('current');
+	} else if (delta < 0){
+		var target = $('.current').data('next');
+		if(target == null) return;
+		$.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: "#" + target
+		});
+		$('.current').removeClass('current');
+		$('#'+target).addClass('current');
+	}
+	return false;
+}
