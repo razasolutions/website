5ce68d26267da39bc4cefbb10168abc280e36c70
ranierelacerda <ranimenezes@hotmail.com>
2016-02-15 16:33:13 -0200

    Css adjusts
    -Some css adjusts;
    -Fixed smooth js .current class handling.

--- js/smooth.js
+++ js/smooth.js
@@ -1,8 +1,9 @@
 $('.smooth').on('click', function() {
     $.smoothScroll({
         scrollElement: $('body'),
-        scrollTarget: this.getAttribute('href')
+        scrollTarget: '#'+this.getAttribute('href')
     });
-    
+    $('.current').removeClass('current');
+	$('#menu-'+this.getAttribute('href')).addClass('current');
     return false;
 });
